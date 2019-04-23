a0f865845211a6d38c53a8d0fcab743bed8571e6
bxs9775 <bxs9775@rit.edu>
2019-04-23 15:28:56 -0400

    Input functionality v.1

--- encoding.js
+++ encoding.js
@@ -9,6 +9,56 @@ let dnaKey;
 let dnaDisp;
 
 /* -------------- Helperfunctions -------------- */
+// onlyUnique function from https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+function onlyUnique(value,index,self){
+  return self.indexOf(value) === index;
+}
+
+function clear(element){
+  element.selectAll('div').remove();
+}
+
+function clearAll(){
+  clear(dnaKey);
+  clear(dnaDisp);
+}
+
+function updateVals(d){
+  console.dir(d);
+  console.dir(this);
+  let newText = this.value | ' ';
+  dnaDisp.selectAll(`.val-${d}`)
+    .text(this.value);
+}
+
+function displayArray(arr){
+  clearAll();
+  let options = arr.filter(onlyUnique);
+  
+  let keyDivs = dnaKey.selectAll('div')
+    .data(options)
+    .enter()
+    .append('div')
+    .attr('class',(d) => `key-div`);
+  keyDivs.append('label')
+    .text((d) => d);
+  keyDivs.append('input')
+    .attr('type','text')
+    .on('change',updateVals);
+  
+  let textDivs = dnaDisp.selectAll('div')
+    .data(arr)
+    .enter()
+    .append('div')
+    .attr('class','text-unit');
+  textDivs.append('div')
+    .attr('class','code')
+    .text((d) => d);
+  textDivs.append('div')
+    .attr('class',(d) => `value val-${d}`)
+    .text(' '); 
+}
+
 function chunks(size,str){
   var length = str.length;
   var chunkArray = [];
@@ -25,6 +75,7 @@ function setup(){
   
   console.dir(chunks(1,encodedMsg));
   console.dir(chunks(3,encodedMsg));
+  displayArray(chunks(3,encodedMsg));
 }
 
 window.onload = setup;
